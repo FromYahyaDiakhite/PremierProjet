f552282ab9a2edb2636721bfd911207cafe1b579
Yahya <i17771.etu@iscae.mr>
2025-05-05 17:12:57 +0000

    mettre de bar de navigation sur l'ensemble page

--- resources/views/etudiant/liste.blade.php
+++ resources/views/etudiant/liste.blade.php
@@ -9,6 +9,28 @@
   <body>
     
     <div class="container text-center">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Scolaire</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/etudiant">Acceuil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/ajouter">Ajouter</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
       <div class="row"> 
         <div class="col s12">
           <h1>CRUD Laravel 10 </h1>
